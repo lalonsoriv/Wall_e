92fc902f0a3fac6fc39c4188e93f63f3b5757eeb
lalonsoriv <130181763+lalonsoriv@users.noreply.github.com>
2023-12-10 04:39:57 -0500

    Update

--- Geo_Wall_E/Stmts/PointStmt.cs
+++ Geo_Wall_E/Stmts/PointStmt.cs
@@ -15,7 +15,7 @@ namespace Geo_Wall_E
         {
             Random random = new();
             List<Type> points = new();
-            int count = random.Next(100, 1000);
+            int count = random.Next(100, 999);
             for (int i = 0; i < count; i++)
             {
                 Point p = new("")
